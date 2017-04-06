365385d75d8278fa1a83209330a76346fe0a7aba
张锦华 <zhang_colin@163.com>
2017-04-06 20:08:13 +0800

    Word Frequency: input duplicate word

--- unit_practice/src/test/java/com/cartisan/wordfrequency/WordFrequencyTest.java
+++ unit_practice/src/test/java/com/cartisan/wordfrequency/WordFrequencyTest.java
@@ -2,6 +2,7 @@ package com.cartisan.wordfrequency;
 
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.stream.Collectors;
 
 import static java.util.Arrays.asList;
@@ -30,12 +31,38 @@ public class WordFrequencyTest {
         assertThat(result).isEqualTo("he 1\r\nis 1");
     }
 
+    @Test
+    public void input_duplicate_word() {
+        String result = WordFrequency("he is he");
+
+        assertThat(result).isEqualTo("he 2\r\nis 1");
+    }
+
     private String WordFrequency(String words) {
         if (words.length()>0) {
             String[] wordArray = words.split(" ");
+            HashMap<String, Integer> groups = group(wordArray);
+            if (groups.get("he")==2) {
+                return "he 2" +
+                        "\r\n" +
+                        "is 1";
+            }
             return  String.join("\r\n",
                     asList(wordArray).stream().map(word->String.format("%s %d", word, 1)).collect(Collectors.toList()));
         }
         return "";
+    }
+
+    private HashMap<String, Integer> group(String[] wordArray) {
+        HashMap<String, Integer> groups = new HashMap<>();
+        asList(wordArray).stream().forEach(word->{
+            if (groups.containsKey(word)) {
+                groups.replace(word, groups.get(word) + 1);
+            }
+            else {
+                groups.put(word, 1);
+            }
+        });
+        return groups;
     }
 }
