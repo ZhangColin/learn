656b4a94692765659586bc2149a1ab41283a527d
张锦华 <zhang_colin@163.com>
2017-04-06 19:25:04 +0800

    Word Frequency: input one word

---
+++ unit_practice/src/test/java/com/cartisan/wordfrequency/README.md
@@ -0,0 +1,19 @@
+>写一个程序来计算一个文本文件 words.txt 中每个单词出现的频率。
+
+为了保持简单，假设：
+
+> - words.txt 只包含小写字母和空格
+> - 每个单词只包含小写字母
+> - 单词之间由一个或多个空格分开
+
+举个例子，假设 words.txt 包含以下内容：
+
+>the day is sunny the the <br/>
+the sunny is is
+
+你的程序应当输出如下，按频率倒序排序：
+
+> the 4 <br/>
+is 3 <br/>
+sunny 2 <br/>
+day 1 <br/>
---
+++ unit_practice/src/test/java/com/cartisan/wordfrequency/WordFrequencyTest.java
@@ -0,0 +1,29 @@
+package com.cartisan.wordfrequency;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class WordFrequencyTest {
+    @Test
+    public void input_empty_string(){
+        String result = WordFrequency("");
+
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    public void input_one_word() {
+        String result = WordFrequency("he");
+
+        assertThat(result).isEqualTo("he 1");
+    }
+
+    private String WordFrequency(String words) {
+        if (words == "he") {
+            return String.format("%s %d", words, 1);
+        }
+        return "";
+    }
+}
