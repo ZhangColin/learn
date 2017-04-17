31f199915010e4bea4a8a8fd7c43113e202bc497
张锦华 <zhang_colin@163.com>
2017-04-17 14:03:38 +0800

    core java: chapter09: LinkedListTest

---
+++ corejava/src/chapter09/LinkedListTest.java
@@ -0,0 +1,44 @@
+package chapter09;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class LinkedListTest {
+    public static void main(String[] args) {
+        List<String> a = new LinkedList<>();
+        a.add("Amy");
+        a.add("Carl");
+        a.add("Erica");
+
+        List<String> b = new LinkedList<>();
+        b.add("Bob");
+        b.add("Doug");
+        b.add("Frances");
+        b.add("Gloria");
+
+        ListIterator<String> aIter = a.listIterator();
+        ListIterator<String> bIter = b.listIterator();
+
+        while (bIter.hasNext()) {
+            if (aIter.hasNext()) {
+                aIter.next();
+            }
+            aIter.add(bIter.next());
+        }
+        System.out.println(a);
+
+        bIter = b.listIterator();
+        while (bIter.hasNext()) {
+            bIter.next();
+            if (bIter.hasNext()) {
+                bIter.next();
+                bIter.remove();
+            }
+        }
+        System.out.println(b);
+
+        a.removeAll(b);
+        System.out.println(a);
+    }
+}
