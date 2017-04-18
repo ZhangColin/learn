b2070679ad73e09fbf1f843283d4496c2c8dc78c
张锦华 <zhang_colin@163.com>
2017-04-18 14:41:22 +0800

    core java: chapter14: ThreadPoolTest

--- corejava/src/chapter14/threadPool/ThreadPoolTest.java
+++ corejava/src/chapter14/threadPool/ThreadPoolTest.java
@@ -1,36 +1,34 @@
-package chapter14;
+package chapter14.threadPool;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.FutureTask;
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
 
-public class FutureTest {
+public class ThreadPoolTest {
     public static void main(String[] args) {
         try (Scanner in = new Scanner(System.in)) {
-            System.out.print("Enter base directory (e.g. /opt/jdk1.8.0/src): ");
+            System.out.print("Enter base directory (e.g. /Users/colin/Java): ");
             String directory = in.nextLine();
             System.out.print("Enter keyword (e.g. volatile): ");
             String keyword = in.nextLine();
 
-            MatchCounter counter = new MatchCounter(new File(directory), keyword);
-            FutureTask<Integer> task = new FutureTask<>(counter);
+            ExecutorService pool = Executors.newCachedThreadPool();
 
-            Thread t = new Thread(task);
-            t.start();
+            MatchCounter counter = new MatchCounter(new File(directory), keyword, pool);
+            Future<Integer> result = pool.submit(counter);
 
-            try{
-                System.out.println(task.get()+" matching files.");
+            try {
+                System.out.println(result.get() + " matching files.");
             } catch (InterruptedException e) {
 
             } catch (ExecutionException e) {
                 e.printStackTrace();
             }
+
+            pool.shutdown();
+
+            int largestPoolSize = ((ThreadPoolExecutor)pool).getLargestPoolSize();
+            System.out.println("largest pool size="+largestPoolSize);
         }
     }
 }
@@ -38,15 +36,18 @@ public class FutureTest {
 class MatchCounter implements Callable<Integer> {
     private File directory;
     private String keyword;
+    private final ExecutorService pool;
+    private int count;
 
-    public MatchCounter(File directory, String keyword) {
+    public MatchCounter(File directory, String keyword, ExecutorService pool) {
         this.directory = directory;
         this.keyword = keyword;
+        this.pool = pool;
     }
 
     @Override
-    public Integer call() throws Exception {
-        int count = 0;
+    public Integer call() {
+        count = 0;
 
         try {
             File[] files = directory.listFiles();
@@ -54,11 +55,9 @@ class MatchCounter implements Callable<Integer> {
 
             for (File file : files) {
                 if (file.isDirectory()) {
-                    MatchCounter counter = new MatchCounter(file, keyword);
-                    FutureTask<Integer> task = new FutureTask<>(counter);
+                    MatchCounter counter = new MatchCounter(file, keyword, pool);
+                    Future<Integer> task = pool.submit(counter);
                     results.add(task);
-                    Thread t = new Thread(task);
-                    t.start();
                 } else {
                     if (search(file)) {
                         count++;
@@ -82,8 +81,8 @@ class MatchCounter implements Callable<Integer> {
     }
 
     public boolean search(File file) {
-        try{
-            try(Scanner in = new Scanner(file, "UTF-8")) {
+        try {
+            try (Scanner in = new Scanner(file, "UTF-8")) {
                 boolean found = false;
                 while (!found && in.hasNextLine()) {
                     String line = in.nextLine();
@@ -94,7 +93,7 @@ class MatchCounter implements Callable<Integer> {
 
                 return found;
             }
-        }catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             return false;
         }
     }
