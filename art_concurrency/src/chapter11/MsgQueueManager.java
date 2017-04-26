8f80a932dda5bb898d9a753e97e18a560a41c093
张锦华 <zhang_colin@163.com>
2017-04-26 16:42:15 +0800

    The Art of Java Concurrency Programming: chapter11

---
+++ art_concurrency/src/chapter11/MsgQueueManager.java
@@ -0,0 +1,32 @@
+package chapter11;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedTransferQueue;
+
+/**
+ * 总消息队列管理
+ */
+public class MsgQueueManager implements IMsgQueue {
+    public final BlockingQueue<Message> messageQueue;
+
+    private MsgQueueManager() {
+        messageQueue = new LinkedTransferQueue<>();
+    }
+
+    public void put(Message msg) {
+        try {
+            messageQueue.put(msg);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public Message take() {
+        try {
+            return messageQueue.take();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        return null;
+    }
+}
