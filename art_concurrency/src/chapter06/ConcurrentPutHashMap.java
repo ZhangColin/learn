3724df0bfa7874f3d4ea46f5279a44ca770cbd4b
张锦华 <zhang_colin@163.com>
2017-04-26 14:58:07 +0800

    The Art of Java Concurrency Programming: chapter06

---
+++ art_concurrency/src/chapter06/ConcurrentPutHashMap.java
@@ -0,0 +1,20 @@
+package chapter06;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class ConcurrentPutHashMap {
+    public static void main(String[] args) throws InterruptedException {
+        final HashMap<String, String> map = new HashMap<>(2);
+
+        Thread t = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                new Thread(() -> {
+                    map.put(UUID.randomUUID().toString(), "");
+                }, "ftf" + i).start();
+            }
+        }, "ftf");
+        t.start();
+        t.join();
+    }
+}
